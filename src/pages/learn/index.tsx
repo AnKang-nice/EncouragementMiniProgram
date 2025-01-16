3e6206a141765b6482e828f73c7a510e043bd539
ankang <2311040049@qq.com>
2025-01-16 18:07:49 +0800

    feat: 处理请求展示

--- src/pages/learn/index.tsx
+++ src/pages/learn/index.tsx
@@ -1,28 +1,70 @@
 import { Text, View } from "@tarojs/components";
-import Taro, { useLoad } from "@tarojs/taro";
-
+import Taro from "@tarojs/taro";
+import { useEffect, useState } from "react";
 import "./index.less";
 
 export default function Learn() {
-  useLoad(() => {
-    console.log("Page loaded.");
-  });
+  const [frontArticleList, setFrontArticleList] = useState([]);
+  const [aiArticleList, setAiArticleList] = useState([]);
+  const getArticle = () => {
+    Taro.request({
+      url: "http://localhost:3636/article/frontHot",
+      method: "GET",
+    }).then((res) => {
+      console.log(res, 5565656565);
+      setFrontArticleList(res.data.data);
+    });
 
-  const gopage = () => {
-    //  跳转https://juejin.cn/
-
-   
+    Promise.all(
+      [
+        Taro.request({
+          url: "http://localhost:3636/article/frontHot",
+          method: "GET",
+        }),
+        Taro.request({
+          url: "http://localhost:3636/article/aiHot",
+          method: "GET",
+        }),
+      ].map((item) => item.catch((err) => err))
+    ).then((res) => {
+      console.log(res, 5565656565);
+      setFrontArticleList(res[0].data.data);
+      setAiArticleList(res[1].data.data);
+    });
   };
 
-  return (
-    <View className='index'>
-      <Text>Learn</Text>
+  const viewContent = (id) => {
+    Taro.navigateTo({
+      url: `/components/webView/index?url=https%3A%2F%2Fjuejin.cn/post/${id}`, // URL 需要进行编码
+    });
+  };
 
-      {/* 跳转链接 */}
-      {/* <View className='link' onClick={gopage}>
-        <Text>Go to Index</Text>
-      </View> */}
-  
+  useEffect(() => {
+    getArticle();
+  }, []);
+
+  return (
+    <View className="index">
+      <Text>Learn</Text>
+      {frontArticleList.map((item, index) => {
+        return (
+          <View key={index}>
+            <Text onClick={() => viewContent(item.content.content_id)}>
+              {item.content.title}
+            </Text>
+          </View>
+        );
+      })}
+      <Text>AI</Text>
+      {aiArticleList.map((item, index) => {
+        return (
+          <View key={index}>
+            <Text onClick={() => viewContent(item.content.content_id)}>
+              {item.content.title}
+            </Text>
+          </View>
+        );
+      })}
     </View>
   );
 }
